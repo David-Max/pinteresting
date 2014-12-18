45e4c51de87476838c94f0e060b752f12b51ff2c
David Uram <david.max.uram@gmail.com>
2014-12-18 14:27:55 -0500

    Customize bootstrap veriables

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
 root "pages#home"
-get "about" => "pages#about"
-get "Home" => "pages#home"
+get "about" => "pages#about" # creates about_path
+get "mission" => "pages#mission"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
